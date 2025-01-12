f8614306df61eddf029d165ad7c089876bbe14ee
atacan <atacandur@icloud.com>
2025-01-12 17:28:08 +0100

    Generated without error

--- Sources/Prepare/main.swift
+++ Sources/Prepare/main.swift
@@ -1,11 +1,12 @@
 import Foundation
 
-func downloadOpenAPI(toFolder folderUrl: URL) {
+func downloadOpenAPI(toFolder folderUrl: URL) async throws {
     let openapiRemoteUrl = URL(string: "https://raw.githubusercontent.com/tdlight-team/tdlight-telegram-bot-api/refs/heads/master/tdlight-api-openapi.yaml")!
-    let content = try! String(contentsOf: openapiRemoteUrl)
+    let content = try String(contentsOf: openapiRemoteUrl)
     // Save yaml files to folderUrl
-    try! content.write(to: folderUrl.appendingPathComponent("original.yaml"), atomically: true, encoding: .utf8)
-    try! content.write(to: folderUrl.appendingPathComponent("openapi.yaml"), atomically: true, encoding: .utf8)
+    try content.write(to: folderUrl.appendingPathComponent("original.yaml"), atomically: true, encoding: .utf8)
+    try content.write(to: folderUrl.appendingPathComponent("openapi.yaml"), atomically: true, encoding: .utf8)
+    print("Downloaded OpenAPI files to \(folderUrl.path)")
 }
 
 let currentFile = URL(fileURLWithPath: #filePath)
@@ -15,6 +16,6 @@ let projectRoot =
     .deletingLastPathComponent()  // Remove 'Prepare'
     .deletingLastPathComponent()  // Remove 'Sources'
 
-downloadOpenAPI(toFolder: projectRoot.appendingPathComponent("Sources/TelegramBotAPI_AHC"))
+try await downloadOpenAPI(toFolder: projectRoot.appendingPathComponent("Sources/TelegramBotAPI_AHC"))
 // Generate code
-try! runCommand("make generate-openapi", workingDirectory: projectRoot.path)
+try runCommand("make generate-openapi", workingDirectory: projectRoot.path)
